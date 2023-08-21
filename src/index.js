99346ac3c0294992405b62ef079633e3885fcd22
suzurini <valloforr@gmail.com>
2023-08-21 20:53:41 +0300

    init: start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let result = false;
+  let n = 0;
+  let bracketsCount = bracketsConfig.length;
+  while (n < str.length){
+    let i = 0;
+    while (i < bracketsCount) {
+      let findBrakets = bracketsConfig[i][0]+bracketsConfig[i][1];
+      i++;
+      if (str.indexOf(findBrakets) !== -1) {
+        let numberChar = str.indexOf(findBrakets);
+        if (numberChar > 1) {
+          str = str.slice(0, numberChar) + str.slice(numberChar + 2);
+          n += 2;
+        }
+        else {
+          str='';
+          result = true;
+        }
+      }
+    }
+  }
+  return result;
 }
